7d7c83f0e51d3a4dc91fada1949f768924ed83e4
Anders Mølmen Høst <andersm_host@hotmail.com>
2019-09-20 12:27:24 +0200

    Moved to new folder

---
+++ exersices/exercise01/comp_to_loop.py
@@ -0,0 +1,22 @@
+#INF200 ex01 Task B
+#Create loop from list comprehension
+
+#Defining squares by list comprehension
+def squares_by_comp(n):
+    return [k**2 for k in range(n) if k % 3 == 1]
+
+#Defining squares by using a for-loop
+def squares_by_loop():
+    squares = []
+    for k in range(n):
+        if k % 3 == 1:
+            squares.append(k ** 2)
+    return squares
+
+"""Checking if the two functions squares_by_comp(n) and squares_by_loop()
+ returns the same value"""
+
+if __name__ == '__main__':
+    n = 50  #adding an input value for n
+    if squares_by_loop() != squares_by_comp(n):
+        print('ERROR!')
---
+++ exersices/exercise01/deck_by_comp.py
@@ -0,0 +1,22 @@
+#INF200 ex01 Task A
+#Create a card deck by list comprehension
+
+#Creating suits and values for cards
+SUITS = ('C', 'S', 'H', 'D')
+VALUES = range(1, 14)
+
+#Creating the card deck by using a for-loop
+def deck_loop():
+    deck = []
+    for suit in SUITS:
+        for val in VALUES:
+            deck.append((suit, val))
+    return deck
+
+#Creating the card deck by list comprehension
+deck_comp = [(suit,val) for suit in SUITS for val in VALUES]
+
+#Checking if the two card decks are the same
+if __name__ == '__main__':
+    if deck_loop() != deck_comp:
+        print('ERROR!')
