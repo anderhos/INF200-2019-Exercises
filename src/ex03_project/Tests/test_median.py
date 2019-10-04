d2f021cf84a090480929d20685ed37ec2ae2d91a
Anders Mølmen Høst <andersm_host@hotmail.com>
2019-10-04 11:40:33 +0200

    Testing one-element list

--- src/ex03_project/Tests/test_median.py
+++ src/ex03_project/Tests/test_median.py
@@ -3,4 +3,24 @@
 __author__ = 'Anders Mølmen Høst'
 __email__ = 'anderhos@nmbu.no'
 
-# INF200. Ex_03-Task A
+# INF200. Ex_03-Task B
+
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
+
+
+def test_single():
+    data1 = [1]
+    assert median(data1) == 1
+    
