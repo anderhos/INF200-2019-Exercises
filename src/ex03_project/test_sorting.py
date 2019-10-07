597ae139486cf562f50a40302d151e382ad835bc
anderhos <54975483+anderhos@users.noreply.github.com>
2019-10-07 09:09:13 +0200

    Update test_sorting.py
    Update file according to comments

--- src/ex03_project/test_sorting.py
+++ src/ex03_project/test_sorting.py
@@ -24,16 +24,6 @@ def bubble_sort(dataset):
     return list_dataset
 
 
-if __name__ == "__main__":
-
-    for data in ((),
-                 (1,),
-                 (1, 3, 8, 12),
-                 (12, 8, 3, 1),
-                 (8, 3, 12, 1)):
-        print('{!s:>15} --> {!s:>15}'.format(data, bubble_sort(data)))
-
-
 def test_empty():
     """Test that the sorting function works for empty list"""
     assert bubble_sort([]) == []
