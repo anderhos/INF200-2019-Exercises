3aee5b808e11b2bbfe1ab6bcb565acd3779a352e
Anders Mølmen Høst <anderhos@nmbu.no>
2019-10-15 14:05:45 +0200

    Update code

--- src/draft_pa01/snakes_and_ladders_draft.py
+++ src/draft_pa01/snakes_and_ladders_draft.py
@@ -1,17 +1,34 @@
 class Board:
     def __init__(self):
-        self.board = [None]*90
-    def is_ladder(self.position):
+        self.board = [None]*90    # 90 positions on board
+    def is_ladder(self, position):
         """
         Returns True if the position is ladder
         """
-        ladder_dict{}
+        """
+        Defining a dictionary for ladders
+        Keys: From	1	8	36	43	49	65	68
+        Values: To	40	10	52	62	79	82	85
+        Correct?
+        """
+        ladder_dict = {
+            1 : 40,
+            8: 10,
+            36: 52,
+            43: 62,
+            49: 79,
+            65: 82,
+            68: 85,
+        }
+        if position in ladder_dict:
+            return value
+    else
     def is_snake(self.position):
         """
         Returns True if the position is snake
         """
 
-    def is_regular
+    def is_regular(self.position)
         """ 
         Returns True if the position is a regular position
         """
