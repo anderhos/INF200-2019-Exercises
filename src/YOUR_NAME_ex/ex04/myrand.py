83c5d0a5bc2e304a5c77f878fd57582b0365adb2
Anders Mølmen Høst <andersm_host@hotmail.com>
2019-10-22 15:28:35 +0200

    draft code

---
+++ src/YOUR_NAME_ex/ex04/myrand.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Anders Mølmen Høst'
+__email__ = 'anderhos@nmbu.no'
+
+
+class LCGRand:
+    def __init__(self, numbers):
+        self.numbers = [
+            rand()
+        ]
+
+    def rand():
+        a = 7**5
+        m = 2**31 - 1
+        r[n+1] = a * r[n] % m
+
+class ListRand:
+    def __init__(self):
