80c8aa0e7b9c97eeff17616a84bbbb3f8d0355b1
Anders Mølmen Høst <andersm_host@hotmail.com>
2019-10-24 13:49:25 +0200

    Code draft and file for ex04

--- src/YOUR_NAME_ex/ex04/myrand.py
+++ src/YOUR_NAME_ex/ex04/myrand.py
@@ -5,15 +5,29 @@ __email__ = 'anderhos@nmbu.no'
 
 
 class LCGRand:
-    def __init__(self, numbers):
-        self.numbers = [
-            rand()
-        ]
 
-    def rand():
-        a = 7**5
-        m = 2**31 - 1
-        r[n+1] = a * r[n] % m
+    def __init__(self, seed):
+        self.seed = seed
+
+    def rand(self):
+        a = 7 ** 5
+        m = 2 ** 31 - 1
+        self.seed = a * self.seed % m
+        return self.seed
+
 
 class ListRand:
-    def __init__(self):
+    def __init__(self, numbers_list):
+        self.numbers_list = numbers_list
+        self.index = -1
+
+    def rand(self):
+        self.index = self.index + 1
+        if self.index < len(self.numbers_list):
+            return self.numbers_list[self.index]
+        else:
+            raise RuntimeError
+
+
+if __name__ == "__main__":
+    print(LCGRand(5))
