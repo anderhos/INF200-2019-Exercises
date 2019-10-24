6456b837882a685d1a270688df4334d5f70717bf
Anders Mølmen Høst <andersm_host@hotmail.com>
2019-10-24 16:52:35 +0200

    Update code, main section not working as it should

--- src/YOUR_NAME_ex/ex04/myrand.py
+++ src/YOUR_NAME_ex/ex04/myrand.py
@@ -4,11 +4,12 @@ __author__ = 'Anders Mølmen Høst'
 __email__ = 'anderhos@nmbu.no'
 
 
+# Implementing the first random number generator class
 class LCGRand:
-
     def __init__(self, seed):
         self.seed = seed
 
+# Generate numbers according to equation
     def rand(self):
         a = 7 ** 5
         m = 2 ** 31 - 1
@@ -16,10 +17,11 @@ class LCGRand:
         return self.seed
 
 
+# Implementing the second random number generator class
 class ListRand:
     def __init__(self, numbers_list):
         self.numbers_list = numbers_list
-        self.index = -1
+        self.index = -1    # Adjusting the index
 
     def rand(self):
         self.index = self.index + 1
@@ -30,4 +32,10 @@ class ListRand:
 
 
 if __name__ == "__main__":
-    print(LCGRand(5))
+    number1 = 4    # Defining a number for LCGRand
+    numbers_list = [1, 2, 3]    # Defining a list of numbers for ListRand
+    for numbers_list in ListRand
+        print(numbers_list.rand())    # Printing numbers, NOT WORKING
+    # How to proceed?
+
+# Checkout examples online
