4f117e5a3880fb3462570ed81a0c5cc8b50b0790
Anders Mølmen Høst <andersm_host@hotmail.com>
2019-11-10 11:44:39 +0100

    Update PEP8

--- src/andershoest_ex/ex05/bounded_sim.py
+++ src/andershoest_ex/ex05/bounded_sim.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import random
 from walker_sim import Walker, Simulation
 
 __author__ = 'Anders Mølmen Høst'
@@ -35,6 +34,7 @@ class BoundedWalker(Walker):
         elif self.get_position() > self.right_limit:
             self.current_position = self.right_limit
 
+
 class BoundedSimulation(Simulation):
     def __init__(self, start, home, seed, left_limit, right_limit):
         """
@@ -57,7 +57,6 @@ class BoundedSimulation(Simulation):
         self.left_limit = left_limit
         self.right_limit = right_limit
 
-
     def single_walk(self):
         bounded_walker = BoundedWalker(self.current_position, self.home,
                                        self.left_limit, self.right_limit)
@@ -74,7 +73,3 @@ if __name__ == "__main__":
                                           right_boundary)
         print("Left boundary:", left_boundary, "-->", "Walk durations:",
               bounded_sim_1.run_simulation(20))
-
-
-
-
