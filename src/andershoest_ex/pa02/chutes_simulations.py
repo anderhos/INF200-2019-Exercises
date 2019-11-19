8db5e161a7237e9f3e779ad9dc705604df8112d0
Anders Mølmen Høst <andersm_host@hotmail.com>
2019-11-19 14:27:04 +0100

    Changed static method goal_reached. Update docstrings.

--- src/andershoest_ex/pa02/chutes_simulations.py
+++ src/andershoest_ex/pa02/chutes_simulations.py
@@ -5,41 +5,52 @@ __email__ = 'anderhos@nmbu.no'
 
 import random
 
+
 class Board:
+    # Ladder dictionary and snake dictionary from pa01
     ladder_dict = {1: 40, 8: 10, 36: 52, 43: 62, 49: 79, 65: 82, 68: 85}
     snake_dict = {24: 5, 33: 3, 42: 30, 56: 37, 64: 27, 74: 12, 87: 70}
-    current_position = 0
-    """
-    Initializes the board.
-
-    Creating a class of the board. Ladders and snakes represented by
-    lists. Position 90 is the endpoint and position 0 is the start
-    position.
-
-    Arguments
-    ---------
-    param start : int 0
-        The first position on the board.
-    param goal : int 90
-        the last position on the board.
-
-    """
+    current_position = 0    # setting the starting position to zero
 
     def __init__(self, ladders=None, snakes=None, goal=90):
+        """
+           Initializes the board.
+
+           Creating a class of the board. Ladders and snakes represented by
+           lists. Position 90 is the endpoint and position 0 is the start
+           position.
+
+           Arguments
+           ---------
+           param start : int 0
+               The first position on the board.
+           param goal : int 90
+               the last position on the board.
+
+           """
         self.ladders = ladders or Board.ladder_dict
         self.snakes = snakes or Board.snake_dict
         self.goal = goal
 
-    def goal_reached(self, position):    # AH question: Method may be static?
-        while not position >= 90:
+    def goal_reached(self, position):
+        """
+        Checks if goal is reached
+
+        :param position:
+        :return: bol
+        """
+        self.current_position = position
+        while not self.current_position >= 90:
             return False
 
     def position_adjustment(self, position):
         """
-        Suggestion. subtract the value of the
-        second index from the first in ladders/snakes
-        numbers from test_base_pa02
+        The method takes as argument the position of the player
+        and returns integer number of positions the player must move
+        forward in case of a ladder or backward in case of a snake
 
+        :param position
+        :return: int
         """
         changed_position = 0
         if position in self.ladders:
@@ -52,6 +63,11 @@ class Board:
 class Player:    # AH: correct? or should it be Player(board)?
 
     def __init__(self, board):
+        """
+        Initializes the player class.
+
+        :param board
+        """
         self.board = Board(Player)
         self.player = Player
 
